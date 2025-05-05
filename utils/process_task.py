0386617ec5cb4fe130c22e50d9c6f4b5a4e18343
Daniel Klionsky <klionskd@yahoo.com>
2025-05-04 20:53:47 -0700

    Update process_task.py

--- utils/process_task.py
+++ utils/process_task.py
@@ -33,7 +33,7 @@ def process_extract_csv_file(row):
     file_path = os.path.join(docker_shared_path, file_name)
 
     # The path as seen by SQL Server (UNC path)
-    sql_server_path = f"\\\\192.168.86.96\\shared\\bulk_files\\{file_name}"
+    sql_server_path = f"\\\\[ip address]\\shared\\bulk_files\\{file_name}"
 
     # Append a query to capture the row count after the bulk insert
     bulk_insert_sql = f"""
